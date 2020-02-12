7e17860830822aed5a9dc622a34d253ac5db201c
mrudoola <47325781+mrudoola@users.noreply.github.com>
2020-02-12 14:35:36 +0530

    Update Book.java

--- JAVA and J2EE/2/Book.java
+++ JAVA and J2EE/2/Book.java
@@ -1,20 +1,24 @@
-public class Book implements Comparable<Book> {
+public class Book implements Comparable<Book> 
+{
 
     String title, author, publisher;
     Double price;
 
-    Book(String t, String aut, String pub, Double pr) {
+    Book(String t, String aut, String pub, Double pr)
+    {
         title = t;
         author = aut;
         publisher = pub;
         price = pr;
     }
 
-    Double getPrice() {
+    Double getPrice() 
+    {
         return price;
     }
 
-    public String toString() {
+    public String toString() 
+    {
         return "Book Details : Title " + title + "\n Author " + author + "\n" + " Publisher " + publisher + "\n"
                 + "price " + price;
     }
